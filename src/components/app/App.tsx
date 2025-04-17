15f6a024ff9c61c6d9f23690e22986d758879644
Andrey <andrey.vi@inbox.ru>
2025-04-17 15:32:21 +0300

    Добавил пробелов

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { data } from '../../utils/data';
-import AppHeader from '../app-header/app-header';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import React from "react";
+import { data } from "../../utils/data";
+import AppHeader from "../app-header/app-header";
+import BurgerConstructor from "../burger-constructor/burger-constructor";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 
 function App() {
   return (
     <>
-    
       <AppHeader />
-      <div style={{ display: 'flex', margin: '50px'}}>
-        <div style={{  width: '50%'}}>
+      <div style={{ display: "flex", margin: "50px" }}>
+        <div style={{ width: "50%" }}>
           <BurgerConstructor />
         </div>
-       <div style={{  width: '50%'}}>
+        <div style={{ width: "50%" }}>
           <BurgerIngredients />
-       </div>
-       
+        </div>
       </div>
     </>
   );
